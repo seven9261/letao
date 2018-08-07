66e0d941dc3a43fd1935364b797f87e4beb333e7
zhangyao <1498893079@qq.com>
2018-08-07 23:56:47 +0800

    共同页面完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -1,4 +1,31 @@
 
+// 5. 判断用户是否登陆, 实现登陆拦截
+//    前端不知道当前用户是否登录, 但是后台知道, 需要访问后台接口, 获取该用户登陆状态
+//    (1) 用户已登陆, 让其继续访问
+//    (2) 如果用户没登陆, 拦截到登陆页
+
+// 一进入页面, 发送 ajax 请求, 获取当前用户登陆状态
+// 如果是登陆页, 不需要登陆, 就可以访问, 不需要判断登陆状态
+if( location.href.indexOf("login.html") === -1  ){
+   // 不是 login.html, 进行登陆拦截判断
+   $.ajax({
+     type: "get",
+     url: "/employee/employeeLogout",
+     dataType: "json",
+     succedss: function( info ){
+        console.log(info);
+        if ( info.success ) {
+          console.log( "已登陆" );
+        }
+  
+        if ( info.error === 400 ) {
+          // 未登录, 拦截到登陆页
+          location.href = "login.html";
+        }
+     }
+   })
+}
+
 
 
 //4 实现进度条功能
@@ -20,4 +47,52 @@ $(document).ajaxStart(function () {
 // 所有ajax 结束  进度条结束
 $(document).ajaxStop(function () {
   NProgress.done();
-}
+});
+
+
+$(function(){
+
+  // 公共功能实现
+  // 1 左侧二级菜单切换
+  $('.lt-aside .category').click(function(e){
+    // alert(1);
+    $('.lt-aside .lt-child').stop().slideToggle();
+    // e.stoppropagation();
+  })
+
+  // 2 点击切换侧边栏
+  $('.icon_menu').click(function() {
+    $('.lt-aside').toggleClass("hidemenu");
+    $('.lt_topbar').toggleClass("hidemenu");
+    $('.lt-main').toggleClass("hidemenu");
+  })
+
+  // 3  点击退出菜单  显示退出模态框
+  $('.icon_logout').click(function(){
+    // 显示模态框
+    $('#logoutModal').modal("show");
+  });
+
+  // 4 点击退出按钮  实现用户退出
+  $('#logoutBtn').click(function(){
+    // 退出需要发送ajax请求, 让服务器端退出, 销毁该用户的登陆状态
+    $.ajax({
+      url: "/employee/employeeLogout",
+      dataType: "json",
+      type: "get",
+      success: function( info ){
+        console.log(info);
+        if ( info.success ) {
+          // 退出成功, 跳转到登陆页
+          location.href = "login.html";
+        }
+
+        
+      }
+    })
+  })
+
+
+
+
+})
